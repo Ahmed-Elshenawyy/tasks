cf729dcb04258bf98c68dbd2480af156403db575
Ahmed-Elshenawyy <aashenawi@gmail.com>
2025-10-25 22:11:10 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,8 +1,8 @@
 def calculate_factorial():
-    n = int(input("Enter a positive integer: "))
+    n = int(input("Enter a positive integer N: "))
     result = 1
     for i in range(1, n + 1):
-        result *= i
-    print("The factorial of", n, "is:", result)
+        result = result * i
+    print("The factorial of", n, "is", result)
 
 calculate_factorial()
